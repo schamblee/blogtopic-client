3ac4d24fd360ef4752051a8ac44e81b31e64c4d0
Stephanie <stephaniejoychamblee@gmail.com>
2018-05-03 15:18:17 -0400

    Add conditional rendering in nav menu

--- src/components/app.js
+++ src/components/app.js
@@ -44,7 +44,7 @@ export class App extends React.Component {
     render() {
         return (
             <div className="app">
-                <Nav />
+                <Nav isLoggedIn={true} />
                 <HeaderBar />
                 <Route exact path="/" component={LandingPage} />
                 <Route exact path="/dashboard" component={Dashboard} />
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,9 +4,9 @@ import {clearAuth} from '../actions/auth';
 import {clearAuthToken} from '../local-storage';
 import './nav.css';
 
-export class Nav extends React.Component {
-
+export class Nav extends React.Component {  
     render() {
+        const isLoggedIn = this.props.isLoggedIn;
         return (
             <nav className="nav" role="navigation">
                 <div id="menuToggle">
@@ -14,12 +14,18 @@ export class Nav extends React.Component {
                     <span></span>
                     <span></span>
                     <span></span>
+                    {isLoggedIn ? (
                     <ul id="menu">
-                        <a href="#"><li>Login</li></a>
-                        <a href="#"><li>Create Account</li></a>
-                        <a href="#"><li>Search</li></a>
-                        <a href="#"><li>Random Topic</li></a>
+                        <a href="/"><li>Log Out</li></a>
+                        <a href="/topics"><li>Search Topics</li></a> 
                     </ul>
+                    ) : (
+                    <ul id="menu">
+                        <a href="/"><li>Log In</li></a>
+                        <a href="/register"><li>Create Account</li></a>
+                        <a href="/topics"><li>Search Topics</li></a> 
+                    </ul>
+                    )}
                 </div>
             </nav>
         );
