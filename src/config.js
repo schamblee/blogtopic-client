e309bf8e2c9b0aa9249c4e4fcbb977db296cf89d
Stephanie <stephaniejoychamblee@gmail.com>
2018-05-18 19:55:47 -0400

    Remove tests

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 
 export const API_BASE_URL = 
-process.env.REACT_APP_API_BASE_URL || 'https://fierce-garden-79213.herokuapp.com';
+process.env.REACT_APP_API_BASE_URL || 'https://fierce-garden-79213.herokuapp.com/api';
